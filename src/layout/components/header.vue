918538f964f902ddb99c88e8cb7157bc3509bfb9
chencheng2021 <1308040062@qq.com>
2021-03-24 14:57:36 +0800

    [UPDATE] 更新了header组件，完成了header组件的简洁风格的编写

--- src/layout/components/header.vue
+++ src/layout/components/header.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="header_wrapper">
+    <div style="width: 40%;">
+      <i class="el-icon-s-platform" style="margin-left: 20px;font-size: 25px;"></i>
+      <label style="font-size: 20px;margin-left: 5px;color: #ffffff;">
+        小学生作业推送管理系统-教师端
+      </label>
+    </div>
+    <div style="width: 60%;">
+      <div style="float:right;width: 300px">
+        <label style="margin-right: 50px;font-size: 18px;color: #ffffff">
+          {{time_desc}}<strong>陈城</strong>老师
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "Header"
+  name: "Header",
+  computed:{
+    time_desc(){
+      let now = new Date()
+      let hours = now.getHours();
+      if (hours >= 6 && hours < 12 ){
+        return '上午好，'
+      }else if (hours >= 12 && hours < 14){
+        return '中午好，'
+      }else if (hours >= 14 && hours < 19){
+        return '下午好，'
+      }else {
+        return '晚上好，'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 #header_wrapper{
+  display: flex;
+  line-height: 60px;
   height: 100%;
   width: 100%;
   background: #95C9FF;
